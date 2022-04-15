af6fd60df39dffd527a39b68d6c50bdf55f32dc9
Eldar <eldar0788@gmail.com>
2022-04-15 18:48:23 +0500

    fix: добавить prettier, индивидуальный текст для кнопок. Убрать ограничение ширины для page

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,12 +1,15 @@
 export default function ImagePopup(props) {
     return (
-        <div className={`popup ${props.card && "popup_active"}`}>
-            <div className="image-popup">
-                <button className="image-popup__close-button close-button hover-transparent" type="button"
-                        onClick={props.onClose}/>
-                <img className="image-popup__image" src={props.card.link} alt=""/>
-                <p className="image-popup__caption">{props.card.name}</p>
-            </div>
+      <div className={`popup ${props.card && "popup_active"}`}>
+        <div className="image-popup">
+          <button
+            className="image-popup__close-button close-button hover-transparent"
+            type="button"
+            onClick={props.onClose}
+          />
+          <img className="image-popup__image" src={props.card?.link} alt={props.card?.name}/>
+          <p className="image-popup__caption">{props.card?.name}</p>
         </div>
-    )
+      </div>
+    );
 }
